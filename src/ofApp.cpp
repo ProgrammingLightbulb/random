fd145af997daf47e9cead3221c2a7dec621a82a7
ProgrammingLightbulb <ProgrammingLightbulb@users.noreply.github.com>
2017-03-27 19:53:16 -0400

    Lab 8 - Random Numbers

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,8 +3,39 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+
+std::random_device rd;
+
+std::mt19937 eng(rd());
+
+float meanX = 1000;
+float sdX = 150;
+float meanY = 800;
+float sdY = 150;
+
+int minX = 75;
+int maxX = 500;
+int minY = 49;
+int maxY = 777;
+
+std::normal_distribution<float> normX(meanX,sdX);
+std::normal_distribution<float> normY(meanY,sdY);
+
+std::uniform_int_distribution<> unifrmX(minX, maxX);
+std::uniform_int_distribution<> unifrmY(minY, maxY);
+
+for (int i = 1; i <= 20; i++){
+
+ X.push_back(unifrmX(eng));
+ Y.push_back(unifrmY(eng));
+
+ X.push_back(normX(eng));
+ Y.push_back(normY(eng));
+
+}
+
+/*X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel*/
 }
 
 //--------------------------------------------------------------
